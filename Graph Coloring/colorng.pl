f704ac80045521d128a6cdcb5d495c597532ac5f
IvanIvanoff <ivan.ivanoff.95@gmail.com>
2016-06-14 20:20:46 +0300

    Better solution

--- Graph Coloring/colorng.pl
+++ Graph Coloring/colorng.pl
@@ -1,5 +1,8 @@
 member(X, [X|_]).
-member(X, [Y|XS]) :- Y \= X, member(X, XS).
+member(X, [_|XS]) :- member(X, XS).
+
+append([],B,B).
+append([A|AS], B,[A|Res]) :- append(AS,B,Res).
 
 disjoint_set(V,[],V).
 disjoint_set(V, V, []).
@@ -11,26 +14,31 @@ cycle3(V, E) :-
 	member([X,Y], E), member([Y,Z], E), member(,[Z,X], E),
 	X \= Y, X \= Z, Y \= Z.
 
-getV(V, []).
-getV([],[]).
-getV([X|V], [[X,Y]|ES]) :-
-	not(member(X, V)),
-	member(Y, V),
-	getV(V, ES).
-getV([Y|V], [[X,Y]|ES]) :-
-	member(X, V), 
-	not(member(Y,V)), 
-	getV(V, ES).
-getV(V,[[X,Y]|ES]) :-
-	member(X,V), 
-	member(Y,V), 
-	getV(V, ES).
-getV([X,Y|ES], [X,Y|ES]) :-
-	not(member(X,V)), 
-	not(member(Y,V)), 
-	getV(V,ES).
+list([]).
+list([_|_]).
+
+flat([],[]).
+flat([H|T], F):- 
+	list(H),
+	flat(H,F1),
+	flat(T,F2),
+	append(F1,F2,F).
+
+flat([H|T], [H|F]) :- 
+	not(list(H)), 
+	flat(T,F).
+
+distinct([],[]).
+distinct([H|T], [H|Res]):-
+	not(member(H, T)),
+	distinct(T,Res).
+
+distinct([H|T], Res):-
+	member(H,T),
+	distinct(T,Res).
 
 p(G) :- 
-	getV(V, G), 
+	flat(G, V1),
+	distinct(V1,V),
 	disjoint_set(V, L, R),
 	not((cycle3(L,G), cycle3(R, G))).
