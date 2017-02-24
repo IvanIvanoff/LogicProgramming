8f7ce4e4a07006633b45f582ffc3f2f1c3d22f11
Ivan Ivanoff <IvanIvanoff@users.noreply.github.com>
2017-02-24 22:28:28 +0200

    Update cartesianprod.pl

--- Cartesian Product/cartesianprod.pl
+++ Cartesian Product/cartesianprod.pl
@@ -1,5 +1,5 @@
-member(X,[X|_]).
-member(X,[_|XS]) :- member(X,XS).
+member(X,[X|_]). %! If the first element in the list is X, then it obviously is a member
+member(X,[_|XS]) :- member(X,XS). %! If we reach here we are sure that the first element is NOT X, but it may be in XS 
 
 p([],[]).
 p([L|LS], [X|XS]) :- member(X,L), p(LS,XS).
